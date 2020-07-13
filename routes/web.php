a5c402a7880b29cf366bb339adc5b05c4225d89e
Reo Sahobby <reoshby@gmail.com>
2020-07-13 22:33:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('index');
-// });
-
-// Route::get('/about', function () {
-//     $nama = "Reo Sahobby";
-//     return view('about', ['nama' => $nama]);
-// });
-
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/mahasiswa', 'MahasiswaController@index');
