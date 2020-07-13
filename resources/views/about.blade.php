59c6ed653d1f8bba553d368bf426bb6ee9174a9a
Reo Sahobby <reoshby@gmail.com>
2020-07-13 19:38:47 +0700

    Update view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout/base')
+
+@section('tittle', 'Halaman About')
+
+@section('container')
+<div class="container">
+    <h1>Hello Selamat datang di website {{$nama}}</h1>
+</div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout/base')
+
+@section('tittle', 'Halaman Home')
+
+@section('container')
+  <div class="container">
+    <h1>Hello, world!</h1>
+  </div>
+  @endsection
